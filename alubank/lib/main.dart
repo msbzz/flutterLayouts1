1842097ed53e8f7f8eaaafd53990ad9876e5611c
Marco <mbarozzi@gmail.com>
2024-02-07 07:36:57 -0300

    criação do box_card.dart que será o card base para os demais widgets

--- alubank/lib/main.dart
+++ alubank/lib/main.dart
@@ -10,7 +10,7 @@ const Alubank({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
+    return const MaterialApp(
       title:'Alubank',
       home: Home(),
     );
--- alubank/lib/screens/home.dart
+++ alubank/lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:alubank/compoments/box_card.dart';
 import 'package:alubank/compoments/sections/header.dart';
 import 'package:flutter/material.dart';
 
@@ -6,10 +7,11 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(  
       body: Column(
         children: <Widget>[
           Header(),
+          BoxCard(boxContent: Text('Primeiro widget')),
         ],
       ),
     );
