0f08a0138340bc96e4c407d4d59b1cd3f06528a9
Marco <mbarozzi@gmail.com>
2024-02-07 07:47:24 -0300

    estilizando o conteiner, criando sombra

--- alubank/lib/compoments/box_card.dart
+++ alubank/lib/compoments/box_card.dart
@@ -9,7 +9,11 @@ const BoxCard({ Key? key, required this.boxContent }) : super(key: key);
   @override
   Widget build(BuildContext context){
     return Container(
-      child:boxContent,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        boxShadow: kElevationToShadow[3],
+        borderRadius:BorderRadius.circular(3), ),
+      child: boxContent,
     );
   }
 }
