2385072845af062ff83124e2fd98df43d673c087
Marco <mbarozzi@gmail.com>
2024-02-07 06:55:33 -0300

    final head

---
+++ alubank/lib/compoments/sections/header.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient:  LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color.fromRGBO(103, 99, 234, 1.0),
+            Color.fromRGBO(155, 105, 254, 1.0),
+            Color.fromRGBO(195, 107, 255, 1.0)
+          ],
+        ),
+        borderRadius : BorderRadius.vertical(bottom: Radius.circular(10)),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                //Text('\$1000', style:TextStyle(fontSize: 28,fontWeight: FontWeight.bold)),
+                Text.rich(
+                  TextSpan(
+                    text: '\$',
+                    style: TextStyle(fontSize: 16),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: '1000.00',
+                          style: TextStyle(
+                              fontSize: 28, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Balanço disponivel',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            Icon(
+              Icons.account_circle,
+              size: 42,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
